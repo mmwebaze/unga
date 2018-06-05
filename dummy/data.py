86583e505d353d83834266b078410f2b5fa7794a
Myke <michael.mwebaze@gmail.com>
2018-06-05 09:35:58 -0400

    added basic authentication to rest api

--- dummy/data.py
+++
@@ -1,6 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-users = {
-    "admin": generate_password_hash("admin"),
-    "doe": generate_password_hash("johndoe")
-}
